9d083933e4c06991f23aee715057982e1e148b2f
maximebouger <bougermaxime@gmail.com>
2022-01-09 08:22:46 +0100

    # create view to create blog

---
+++ resources/views/createblog.blade.php
@@ -0,0 +1,5 @@
+<div style="margin-left: 3%; margin-top: 3%">
+    <h2>Hello {{ Auth::user()->name}} !</h2>
+    test du create blog est du create blog est du create blog est du create blog
+
+</div>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,14 +3,11 @@
 
     <div style="margin-top: 0px; box-shadow: 9px 1px 5px -1px rgba(209,209,209,0.53);
     -webkit-box-shadow: 9px 1px 5px -1px rgba(209,209,209,0.53);
-    -moz-box-shadow: 9px 1px 5px -1px rgba(209,209,209,0.53); height:100%">
+    -moz-box-shadow: 9px 1px 5px -1px rgba(209,209,209,0.53); height:100%; display:flex;">
 
         <div
             style=" width:16%; background-color:rgb(31, 31, 31); height:900px; margin-left:0px; display:flex;flex-direction:column">
-            <div style="color: white; border:none; margin-left:6%;margin-top:7%;">
-                <span class="fa fa-plus-square-o"></span><a href="#" class="w3-bar-item w3-button"
-                    style="color: white; border:none; margin-left:6%;">Create a new Blog</a><br>
-            </div>
+
 
             <div style="color: white; border:none; margin-left:6%;margin-top:7%;">
                 <span class="fa fa-eye"></span><a href="#" class="w3-bar-item w3-button"
@@ -22,6 +19,10 @@
                     style="color: white; border:none; margin-left:6%;">Parameters</a><br>
             </div>
 
+        </div>
+
+        <div>
+            @include('createblog')
         </div>
 
     </div>
