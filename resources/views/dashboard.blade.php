36499cc5395eb5d5a7cff64480f2fc20ed30c074
maximebouger <bougermaxime@gmail.com>
2022-01-09 07:49:23 +0100

    front end adjustments

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,13 +1,28 @@
 <x-app-layout>
 
 
-    <div style="margin-top: 0px;">
+    <div style="margin-top: 0px; box-shadow: 9px 1px 5px -1px rgba(209,209,209,0.53);
+    -webkit-box-shadow: 9px 1px 5px -1px rgba(209,209,209,0.53);
+    -moz-box-shadow: 9px 1px 5px -1px rgba(209,209,209,0.53);">
 
-            <div  style="width:20%; background-color:rgb(21, 21, 21); height:600px; margin-left:0px;">
-                <a href="#" class="w3-bar-item w3-button">Link 1</a>
-                <a href="#" class="w3-bar-item w3-button">Link 2</a>
-                <a href="#" class="w3-bar-item w3-button">Link 3</a>
-              </div>
+        <div
+            style=" width:16%; background-color:rgb(31, 31, 31); height:600px; margin-left:0px; display:flex;flex-direction:column">
+            <div style="color: white; border:none; margin-left:6%;margin-top:7%;">
+                <span class="fa fa-plus-square-o"></span><a href="#" class="w3-bar-item w3-button"
+                    style="color: white; border:none; margin-left:6%;">Create a new Blog</a><br>
+            </div>
+
+            <div style="color: white; border:none; margin-left:6%;margin-top:7%;">
+                <span class="fa fa-eye"></span><a href="#" class="w3-bar-item w3-button"
+                    style="color: white; border:none; margin-left:6%;">My Blogs</a><br>
+            </div>
+
+            <div style="color: white; border:none; margin-left:6%;margin-top:7%;">
+                <span class="fa fa-cog"></span><a href="#" class="w3-bar-item w3-button"
+                    style="color: white; border:none; margin-left:6%;">Parameters</a><br>
+            </div>
+
+        </div>
 
     </div>
 </x-app-layout>
@@ -20,99 +35,104 @@
 </head>
 
 <body class="is-preload">
-	<!-- Wrapper -->
-	<div id="wrapper">
+    <!-- Wrapper -->
+    <div id="wrapper">
 
-		<!-- Header -->
-		<header id="header">
+        <!-- Header -->
+        <header id="header">
 
-		</header>
+        </header>
 
-		<!-- Menu -->
-		<nav id="menu">
-			<h2>Menu</h2>
-			<ul>
-				<li><a href="index.html" class="active">Dashboard</a></li>
+        <!-- Menu -->
+        <nav id="menu">
+            <h2>Menu</h2>
+            <ul>
+                <li><a href="index.html" class="active">Dashboard</a></li>
 
-				<li><a href="blog.html">Blog</a></li>
+                <li><a href="blog.html">Blog</a></li>
 
-				<li><a href="about.html">About</a></li>
+                <li><a href="about.html">About</a></li>
 
-				<li><a href="team.html">Authors</a></li>
+                <li><a href="team.html">Authors</a></li>
 
-				<li><a href="contact.html">Contact Us</a></li>
-			</ul>
-		</nav>
+                <li><a href="contact.html">Contact Us</a></li>
+            </ul>
+        </nav>
 
 
 
-		<!-- Footer -->
-		<footer id="footer">
-			<div class="inner">
-				<section>
-					<h2>Contact Us</h2>
-					<form method="post" action="#">
-						<div class="fields">
-							<div class="field half">
-								<input type="text" name="name" id="name" placeholder="Name" />
-							</div>
+        <!-- Footer -->
+        <footer id="footer">
+            <div class="inner">
+                <section>
+                    <h2>Contact Us</h2>
+                    <form method="post" action="#">
+                        <div class="fields">
+                            <div class="field half">
+                                <input type="text" name="name" id="name" placeholder="Name" />
+                            </div>
 
-							<div class="field half">
-								<input type="text" name="email" id="email" placeholder="Email" />
-							</div>
+                            <div class="field half">
+                                <input type="text" name="email" id="email" placeholder="Email" />
+                            </div>
 
-							<div class="field">
-								<input type="text" name="subject" id="subject" placeholder="Subject" />
-							</div>
+                            <div class="field">
+                                <input type="text" name="subject" id="subject" placeholder="Subject" />
+                            </div>
 
-							<div class="field">
-								<textarea name="message" id="message" rows="3" placeholder="Notes"></textarea>
-							</div>
+                            <div class="field">
+                                <textarea name="message" id="message" rows="3" placeholder="Notes"></textarea>
+                            </div>
 
-							<div class="field text-right">
-								<label>&nbsp;</label>
+                            <div class="field text-right">
+                                <label>&nbsp;</label>
 
-								<ul class="actions">
-									<li><input type="submit" value="Send Message" class="primary" /></li>
-								</ul>
-							</div>
-						</div>
-					</form>
-				</section>
-				<section>
-					<h2>Contact Info</h2>
+                                <ul class="actions">
+                                    <li><input type="submit" value="Send Message" class="primary" /></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </form>
+                </section>
+                <section>
+                    <h2>Contact Info</h2>
 
-					<ul class="alt">
-						<li><span class="fa fa-envelope-o"></span> <a href="#">contact@company.com</a></li>
-						<li><span class="fa fa-phone"></span> +1 333 4040 5566 </li>
-						<li><span class="fa fa-map-pin"></span> 212 Barrington Court New York, ABC 10001 United States of America</li>
-					</ul>
+                    <ul class="alt">
+                        <li><span class="fa fa-envelope-o"></span> <a href="#">contact@company.com</a></li>
+                        <li><span class="fa fa-phone"></span> +1 333 4040 5566 </li>
+                        <li><span class="fa fa-map-pin"></span> 212 Barrington Court New York, ABC 10001 United States
+                            of America</li>
+                    </ul>
 
-					<h2>Follow Us</h2>
+                    <h2>Follow Us</h2>
 
-					<ul class="icons">
-						<li><a href="#" class="icon style2 fa-twitter"><span class="label">Twitter</span></a></li>
-						<li><a href="#" class="icon style2 fa-facebook"><span class="label">Facebook</span></a></li>
-						<li><a href="#" class="icon style2 fa-instagram"><span class="label">Instagram</span></a></li>
-						<li><a href="#" class="icon style2 fa-linkedin"><span class="label">LinkedIn</span></a></li>
-					</ul>
-				</section>
+                    <ul class="icons">
+                        <li><a href="#" class="icon style2 fa-twitter"><span class="label">Twitter</span></a>
+                        </li>
+                        <li><a href="#" class="icon style2 fa-facebook"><span class="label">Facebook</span></a>
+                        </li>
+                        <li><a href="#" class="icon style2 fa-instagram"><span
+                                    class="label">Instagram</span></a></li>
+                        <li><a href="#" class="icon style2 fa-linkedin"><span class="label">LinkedIn</span></a>
+                        </li>
+                    </ul>
+                </section>
 
-				<ul class="copyright">
-					<li>Copyright © 2020 Company Name </li>
-					<li>Template by: <a href="https://www.phpjabbers.com/">PHPJabbers.com</a></li>
-				</ul>
-			</div>
-		</footer>
+                <ul class="copyright">
+                    <li>Copyright © 2020 Company Name </li>
+                    <li>Template by: <a href="https://www.phpjabbers.com/">PHPJabbers.com</a></li>
+                </ul>
+            </div>
+        </footer>
 
-	</div>
+    </div>
 
-	<!-- Scripts -->
-	<script src="assets/js/jquery.min.js"></script>
-	<script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>
-	<script src="assets/js/jquery.scrolly.min.js"></script>
-	<script src="assets/js/jquery.scrollex.min.js"></script>
-	<script src="assets/js/main.js"></script>
+    <!-- Scripts -->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/jquery.scrolly.min.js"></script>
+    <script src="assets/js/jquery.scrollex.min.js"></script>
+    <script src="assets/js/main.js"></script>
 
 </body>
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,101 +1,111 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+</head>
 
-            <!-- Page Heading -->
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-
-                    <title>Area17 - Dashboard</title>
-	<meta charset="utf-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-	<link rel="stylesheet" href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}" />
-	<link rel="stylesheet" href="{{ asset('assets/css/main.css') }}" />
-	<noscript>
-		<link rel="stylesheet" href="{{ asset('assets/css/noscript.css') }}" />
-	</noscript>
-    <div class="inner" style="display: flex; flex-direction:row; justify-content: space-between">
-
-        <!-- Logo -->
-        <a class="logo" style="border: none;">
-            <img src="images/area17-log.jpg"  alt=""> <span class="title">AREA 17 - Blogging Platform Welcome Username</span>
-        </a>
+<body class="font-sans antialiased">
+    <div class="min-h-screen bg-gray-100">
 
 
-        <div class="hidden sm:flex sm:items-center sm:ml-6">
-            <x-dropdown align="right" width="48">
-                <x-slot name="trigger">
-                    <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
-                        <div>{{ Auth::user()->name }}</div>
+        <!-- Page Heading -->
+        <header class="bg-white shadow">
+            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
 
-                        <div class="ml-1">
-                            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                            </svg>
+                <title>Area17 - Dashboard</title>
+                <meta charset="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+                <link rel="stylesheet" href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}" />
+                <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}" />
+                <noscript>
+                    <link rel="stylesheet" href="{{ asset('assets/css/noscript.css') }}" />
+                </noscript>
+                <div class="inner" style="display: flex; flex-direction:row; justify-content: space-between">
+
+                    <!-- Logo -->
+                    <a class="logo" style="border: none;">
+                        <div style="display: flex;  align-items: center;">
+                            <img src="images/area17-log.jpg" alt=""> <span class="title">AREA 17 - Blogging
+                                Platform Welcome Username</span>
                         </div>
-                    </button>
-                </x-slot>
 
-                <x-slot name="content" style="border: none;">
-                    <!-- Authentication -->
-                    <form method="POST" action="{{ route('logout') }}" style="margin: 0px;">
-                        @csrf
+                    </a>
 
-                        <x-dropdown-link :href="route('logout')"
-                                onclick="event.preventDefault();
+
+                    <div class="hidden sm:flex sm:items-center sm:ml-6">
+                        <x-dropdown align="right" width="48">
+                            <x-slot name="trigger">
+                                <button
+                                    class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                                    <div>{{ Auth::user()->name }}</div>
+
+                                    <div class="ml-1">
+                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 20 20">
+                                            <path fill-rule="evenodd"
+                                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                clip-rule="evenodd" />
+                                        </svg>
+                                    </div>
+                                </button>
+                            </x-slot>
+
+                            <x-slot name="content" style="border: none;">
+                                <!-- Authentication -->
+                                <form method="POST" action="{{ route('logout') }}" style="margin: 0px;">
+                                    @csrf
+
+                                    <x-dropdown-link :href="route('logout')" onclick="event.preventDefault();
                                             this.closest('form').submit();">
-                            {{ __('Log Out') }}
-                        </x-dropdown-link>
-                    </form>
-                </x-slot>
-            </x-dropdown>
-        </div>
+                                        {{ __('Log Out') }}
+                                    </x-dropdown-link>
+                                </form>
+                            </x-slot>
+                        </x-dropdown>
+                    </div>
 
 
-        <!-- Nav -->
-        <!-- Menu -->
-		<nav id="menu">
-			<h2>Menu</h2>
-			<ul>
-				<li><a href="index.html" class="active">Dashboard</a></li>
+                    <!-- Nav -->
+                    <!-- Menu -->
+                    <nav id="menu">
+                        <h2>Menu</h2>
+                        <ul>
+                            <li><a href="index.html" class="active">Dashboard</a></li>
 
-				<li><a href="blog.html">Blog</a></li>
+                            <li><a href="blog.html">Blog</a></li>
 
-				<li><a href="about.html">About</a></li>
+                            <li><a href="about.html">About</a></li>
 
-				<li><a href="team.html">Authors</a></li>
+                            <li><a href="team.html">Authors</a></li>
 
-				<li><a href="contact.html">Contact Us</a></li>
-			</ul>
-		</nav>
+                            <li><a href="contact.html">Contact Us</a></li>
+                        </ul>
+                    </nav>
 
-    </div>
                 </div>
-            </header>
+            </div>
+        </header>
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-                <div> Content dans le slot ceci est un test</div>
-            </main>
-        </div>
-    </body>
+        <!-- Page Content -->
+        <main>
+            {{ $slot }}
+            <div> Content dans le slot ceci est un test</div>
+        </main>
+    </div>
+</body>
+
 </html>
