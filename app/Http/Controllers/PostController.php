95d19e71ea4d57a5efdb4120c66e34376ef9c722
maximebouger <bougermaxime@gmail.com>
2022-01-11 23:05:59 +0100

    Migration Post and slug / PostController store new post in database

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,16 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
     public function create(Request $request)
     {
-        dump('create post controller');
-        dump($request->all());
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|string|max:50',
+            'post_content' => 'required',
+            'author' => 'required',
+        ]);
 
-        return new JsonResponse([],200);
+        if ($validator->fails()) {
+            return new JsonResponse(['a required field is missing'],400);
+        }
+
+        $author = User::findOrFail($request->get('author'));
+
+        //create new post
+
+        Post::create([
+            'title' => $request->get('title'),
+            'user_id' => $author->id,
+            'user_name' => $author->slug_name,
+            'post_content' => $request->get('post_content')
+        ]);
+
+        //return Redirect::to('/dashboard')->with('success','The post "'.$request->get('title').'" has been created !');
+        return view('dashboard', array('success' => 'The post "'.$request->get('title').'" has been created !'));
+       //return new JsonResponse([],200);
     }
 }
