bb2c396332765da70d34c67bf2df6f257f9175d8
maximebouger <bougermaxime@gmail.com>
2022-02-05 12:09:19 +0100

    add bubble banner

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -95,14 +95,9 @@
                     </ol>
                     <div class="carousel-inner">
                         <div class="carousel-item active">
-                            <img class="d-block w-100" src="images/area-17.jpg" alt="First slide">
+                            <img class="d-block w-100" src="images/Bubble Blog.gif" alt="First slide">
                         </div>
-                        <div class="carousel-item">
-                            <img class="d-block w-100" src="images/slider-image-2-1920x700.jpg" alt="Second slide">
-                        </div>
-                        <div class="carousel-item">
-                            <img class="d-block w-100" src="images/slider-image-3-1920x700.jpg" alt="Third slide">
-                        </div>
+
                     </div>
                     <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
                         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
